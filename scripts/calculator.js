a39201a46818b89257ed0c6dccb5a675f1353683
GimboidGit <alan.webster@hotmail.com>
2022-08-16 16:49:04 +0100

    Update calculator.js
    Implemented "Operate" function.
    Controller for calling appropriate function for
        a given operator and two numbers.

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -17,3 +17,21 @@ function Divide(a, b)
 {
     return a / b;
 }
+
+function Operate(operation, a, b)
+{
+    switch (operation)
+    {
+        case "ADD":
+            return Add(a, b);
+
+        case "SUB":
+            return Subtract(a, b);
+
+        case "MULT":
+            return Multiply(a, b);
+
+        case "DIVI":
+            return Divide(a, b);
+    }
+}
