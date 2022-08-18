800a7b24ceeb43620e538205f8b5d0ba4646f981
GimboidGit <alan.webster@hotmail.com>
2022-08-18 09:02:50 +0100

    Update index.html/main.js
    index.html:
    - Removed value attribute and value from input tag
    
    main.js:
    - Region added: DOM Interation
    - Global variables added for future use
    - New Function: ButtonClick
    -- Test code here at the moment
    -- Real purpose is for taking any button click
        and acting on it accordingly
    - New Function: UpdateDisplay
    -- Updates the input field on calculator with
        given value.

--- scripts/main.js
+++ scripts/main.js
@@ -16,3 +16,40 @@ import { Operate } from "./calculator.js";
 
 //#endregion Code Tests
 
+
+//#region DOM Interaction
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach
+    (button => button.addEventListener("click", ButtonClick));
+
+//#endregion DOM Interaction
+
+
+const DEFAULT_VALUE = 0;
+
+let displayValue = DEFAULT_VALUE;
+let currentSum = DEFAULT_VALUE;
+
+let leftOperand = null;
+let operator = null;
+let rightOperand = null;
+
+UpdateDisplay(DEFAULT_VALUE);
+
+
+
+//Single function for all buttons.
+//We'll read the button's text as a means
+//of telling what the action to take is.
+function ButtonClick(event)
+{
+    alert(event.target.innerText);
+}
+
+
+function UpdateDisplay(value)
+{
+    const displayField = document.getElementById("disp");
+    displayField.setAttribute("value", value);
+}
