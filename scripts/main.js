69703398b7b83aeb0c2eaf2d5cd45b0694d6bbbd
GimboidGit <alan.webster@hotmail.com>
2022-09-02 14:01:47 +0100

    Update main.js
    Removed code testing region.

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,5 @@
 import { Operate } from "./calculator.js";
 
-//#region Code Tests
-
-// const body = document.querySelector("body");
-// const button = document.createElement("button")
-// button.textContent = "Test Calc";
-// button.addEventListener("click", Tester);
-// body.appendChild(button);
-
-// function Tester()
-// {
-//     let res = Operate("ADD", 2, 8);
-//     alert(res);
-// }
-
-//#endregion Code Tests
-
-
 //#region DOM Interaction
 
 const buttons = document.querySelectorAll("button");
